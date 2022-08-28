78891b54c782bc0e57f166cc8a41c86835e4a93c
Yasin Ekin <ekinyasin90@gmail.com>
2022-08-28 16:49:20 +0300

    Movita Automation project has been started

---
+++ src/test/java/pages/MovitaPage.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class MovitaPage {
+
+    public MovitaPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+    @FindBy(xpath = "//div[text()=\"Kurumsal\"]")
+   public WebElement kurumsalMenu;
+
+    @FindBy(xpath = "(//ul[@class=\"sub-menu-container\"])[1]")
+    public List<WebElement> subKurumsalMenu;
+
+    @FindBy (id = "logo")
+    public WebElement movitaLogo;
+
+    @FindBy(xpath = "//div[text()=\"Mobil Vasıta İzleme Takip Sistemi\"]")
+    public WebElement mainTextTurkish;
+
+
+}
---
+++ src/test/java/stepdefinitions/MovitaStepDefinitions.java
@@ -0,0 +1,38 @@
+package stepdefinitions;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import pages.MovitaPage;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+
+public class MovitaStepDefinitions {
+
+    MovitaPage movita=new MovitaPage();
+
+    @Given("User navigates to {string} page")
+    public void user_navigates_to_page(String homePage) {
+        Driver.getDriver().get(ConfigurationReader.getProperty(homePage));
+    }
+    @When("clicks on movita logo")
+    public void clicks_on_movita_logo() {
+       movita.movitaLogo.click();
+    }
+    @Then("User should navigate to homepage")
+    public void user_should_navigate_to_homepage() {
+      String expectedUrl="https://movita.com.tr/";
+      String actualUrl=Driver.getDriver().getCurrentUrl();
+        Assert.assertEquals("Urls are not matched.User can't navigate to homepage",expectedUrl,actualUrl);
+    }
+    @Then("Verify if Mobil Vasıta İzleme Takip Sistemi displayed")
+    public void verify_if_mobil_vasıta_izleme_takip_sistemi_displayed() {
+        ReusableMethods.waitForVisibility(movita.mainTextTurkish,3);
+       Assert.assertTrue(movita.mainTextTurkish.isDisplayed());
+    }
+
+}
