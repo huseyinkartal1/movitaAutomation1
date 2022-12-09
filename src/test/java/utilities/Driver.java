033d982663581ee60297f3969fea9aa4930e6c3e
hkartal <98432117+archer1903@users.noreply.github.com>
2022-12-09 23:18:56 +0300

    chromeoptions headless-disaple gpu added

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -21,18 +21,20 @@ public class Driver {
                     WebDriverManager.chromedriver().setup();
 
 
-                /*
+
                     ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--headless");
-                    options.addArguments("--start-maximized"); // open Browser in maximized mode
+                    options.addArguments("headless");
+                    options.addArguments("disable-gpu"); // applicable to windows os only
+                    /*options.addArguments("--start-maximized"); // open Browser in maximized mode
                     options.addArguments("disable-infobars"); // disabling infobars
                     options.addArguments("--disable-extensions"); // disabling extensions
-                    options.addArguments("--disable-gpu"); // applicable to windows os only
-                    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                    options.addArguments("--no-sandbox"); // Bypass OS security model
-                    driver = new ChromeDriver(options);*/
 
-                    driver=new ChromeDriver();
+                    options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                    options.addArguments("--no-sandbox"); // Bypass OS security model*/
+
+                    driver = new ChromeDriver(options);
+
+                    //driver=new ChromeDriver();
                     driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
                     break;
