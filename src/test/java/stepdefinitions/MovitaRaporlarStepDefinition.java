fd2e25bf71778070b4c17cc835a1f7d932e80535
hkartal <98432117+archer1903@users.noreply.github.com>
2022-12-09 16:11:35 +0300

    smoketest reports deleted waitforvisibility

--- src/test/java/stepdefinitions/MovitaRaporlarStepDefinition.java
+++ src/test/java/stepdefinitions/MovitaRaporlarStepDefinition.java
@@ -60,13 +60,13 @@ public class MovitaRaporlarStepDefinition extends ReusableMethods {
     @Then("click  on Rapor al")
     public void clickOnRaporAl() {
         String expectedURL = Driver.getDriver().getCurrentUrl();
-        click(movita.menuRaporAlButton);
+        movita.menuRaporAlButton.click();
         assertNotEquals(Driver.getDriver().getCurrentUrl(), expectedURL);
     }
 
     @And("click on Yakıt Entegrasyon Raporu")
     public void clickOnYakıtEntegrasyonRaporu() {
-        click(movita.menuYakitEntegrasyonRaporu);
+        movita.menuYakitEntegrasyonRaporu.click();
     }
 
 
