32510f1df181673c59c0984f06d6696f68d63ef1
hkartal <98432117+archer1903@users.noreply.github.com>
2022-12-09 12:07:02 +0300

    ChromeOptions commented

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -20,7 +20,7 @@ public class Driver {
                     //System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
                     WebDriverManager.chromedriver().setup();
 
-                    ChromeOptions options = new ChromeOptions();
+                  /*  ChromeOptions options = new ChromeOptions();
                     options.addArguments("--headless");
                     options.addArguments("start-maximized"); // open Browser in maximized mode
                     options.addArguments("disable-infobars"); // disabling infobars
@@ -28,10 +28,10 @@ public class Driver {
                     options.addArguments("--disable-gpu"); // applicable to windows os only
                     options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
                     options.addArguments("--no-sandbox"); // Bypass OS security model
-                    driver = new ChromeDriver(options);
-                   // driver=new ChromeDriver();
+                    driver = new ChromeDriver(options);*/
 
-                    // driver.manage().window().maximize();
+                    driver=new ChromeDriver();
+                    driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
                     break;
                 case "firefox":
