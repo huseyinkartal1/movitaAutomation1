f59b67cd4b1ac9f93b11bfb111c54061997317f6
hkartal <huseyin86kartal@gmail.com>
2022-12-26 13:46:03 +0300

    test

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -17,13 +17,14 @@ public class Driver {
             String browser=ConfigurationReader.getProperty("browser");
             switch (browser){
                 case "chrome":
-                    System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
-                    //WebDriverManager.chromedriver().setup();
-
-
+                    //System.setProperty("webdriver.chrome.driver","/usr/bin/chromedriver");
+                    WebDriverManager.chromedriver().setup();
 
                     ChromeOptions options = new ChromeOptions();
                     options.addArguments("headless");
+                    driver = new ChromeDriver(options);
+/*
+
                     options.addArguments("disable-gpu"); // applicable to windows os only
                     /*options.addArguments("--start-maximized"); // open Browser in maximized mode
                     options.addArguments("disable-infobars"); // disabling infobars
@@ -32,10 +33,10 @@ public class Driver {
                     options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
                     options.addArguments("--no-sandbox"); // Bypass OS security model*/
 
-                    driver = new ChromeDriver(options);
 
-                    //driver=new ChromeDriver();
-                    driver.manage().window().maximize();
+
+                //    driver=new ChromeDriver();
+                  //  driver.manage().window().maximize();
                     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
                     break;
                 case "firefox":
