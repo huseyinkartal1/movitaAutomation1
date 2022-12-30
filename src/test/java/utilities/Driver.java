448fd7716dd5576e924dee417c86591540dbcc8b
hkartal <huseyin86kartal@gmail.com>
2022-12-30 03:31:46 +0300

    just window size

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -20,17 +20,17 @@ public class Driver {
                     //System.setProperty("webdriver.chrome.driver","Drivers/chromedriver.exe");
                     WebDriverManager.chromedriver().setup();
                     ChromeOptions options = new ChromeOptions();
-                    options.addArguments("--headless");
-                 // options.addArguments("--window-size=1920,900");
+                  //  options.addArguments("--headless");
+                  options.addArguments("--window-size=1920,900");
 
 
 
-                  //  options.addArguments("--start-maximized"); // open Browser in maximized mode
+                 /*  options.addArguments("--start-maximized"); // open Browser in maximized mode
                     options.addArguments("disable-infobars"); // disabling infobars
                     options.addArguments("--disable-extensions"); // disabling extensions
                     options.addArguments("--disable-gpu"); // applicable to windows os only
                     options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-                    options.addArguments("--no-sandbox"); // Bypass OS security model
+                    options.addArguments("--no-sandbox"); // Bypass OS security model*/
                     driver = new ChromeDriver(options);
 
 
